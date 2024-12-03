6662b175624352471deae748349cf9c727c3884a
xiangchen02 <18351967270@163.com>
2024-12-03 19:33:26 +0800

    feat: upgrade to NDK 21 - Update build scripts and workflow to use NDK r21e with clang compiler

--- build_arm.sh
+++ build_arm.sh
@@ -3,13 +3,15 @@ cd luajit-2.1/src
 # Android/ARM, armeabi-v7a (ARMv7 VFP), Android 4.0+ (ICS)
 NDK=$ANDROID_NDK_HOME
 NDKABI=21
-NDKTRIPLE=arm-linux-androideabi
-NDKP=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/$NDKTRIPLE-
+NDKTRIPLE=armv7a-linux-androideabi$NDKABI
+NDKP=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/
+NDKCC=$NDKP/clang
+NDKCXX=$NDKP/clang++
 NDKF="--sysroot $NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot"
 NDKARCH="-march=armv7-a -mfloat-abi=softfp -Wl,--fix-cortex-a8"
 
 make clean
-make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF $NDKARCH"
+make HOST_CC="gcc -m32" CROSS=$NDKP CROSS_CC="$NDKCC -target $NDKTRIPLE" TARGET_SYS=Linux TARGET_FLAGS="$NDKF $NDKARCH"
 cp ./libluajit.a ../../android/jni/libluajit.a
 make clean
 
--- build_arm64.sh
+++ build_arm64.sh
@@ -3,13 +3,15 @@ cd luajit-2.1/src
 # Android/ARM, armeabi-v7a (ARMv7 VFP), Android 4.0+ (ICS)
 NDK=$ANDROID_NDK_HOME
 NDKABI=21
-NDKTRIPLE=aarch64-linux-android
-NDKP=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/$NDKTRIPLE-
+NDKTRIPLE=aarch64-linux-android$NDKABI
+NDKP=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/
+NDKCC=$NDKP/clang
+NDKCXX=$NDKP/clang++
 NDKF="--sysroot $NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot"
 NDKARCH="-DLJ_ABI_SOFTFP=0 -DLJ_ARCH_HASFPU=1"
 
 make clean
-make HOST_CC="gcc -m64" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF $NDKARCH"
+make HOST_CC="gcc -m64" CROSS=$NDKP CROSS_CC="$NDKCC -target $NDKTRIPLE" TARGET_SYS=Linux TARGET_FLAGS="$NDKF $NDKARCH"
 cp ./libluajit.a ../../android/jni/libluajit.a
 make clean
 
