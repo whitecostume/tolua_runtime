ee54fb3fa2a28fb3401c7025c7db17f97859b3bf
xiangchen02 <18351967270@163.com>
2024-12-03 20:38:21 +0800

    feat: 添加 release 编译优化选项，启用 O3 优化并禁用调试信息

--- build_arm.sh
+++ build_arm.sh
@@ -12,13 +12,13 @@ NDKCC=$NDKP/armv7a-linux-androideabi$NDKABI-clang
 NDKCROSS=$NDKP/arm-linux-androideabi-
 
 make clean
-make HOST_CC="clang -m32" CROSS=$NDKCROSS STATIC_CC=$NDKCC DYNAMIC_CC="$NDKCC -fPIC" TARGET_LD=$NDKCC TARGET_AR="$NDKP/llvm-ar rcus" TARGET_STRIP="$NDKP/llvm-strip" TARGET_SYS=Linux TARGET_FLAGS="--sysroot $NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot $NDKARCH"
+make HOST_CC="clang -m32" CROSS=$NDKCROSS STATIC_CC=$NDKCC DYNAMIC_CC="$NDKCC -fPIC -O3" TARGET_LD=$NDKCC TARGET_AR="$NDKP/llvm-ar rcus" TARGET_STRIP="$NDKP/llvm-strip" TARGET_SYS=Linux TARGET_FLAGS="--sysroot $NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot $NDKARCH"
 cp ./libluajit.a ../../android/jni/libluajit.a
 make clean
 
 cd ../../android
 $NDK/ndk-build clean APP_ABI="armeabi-v7a,x86,arm64-v8a" APP_PLATFORM=android-21
-$NDK/ndk-build APP_ABI="armeabi-v7a" APP_PLATFORM=android-21
+$NDK/ndk-build APP_ABI="armeabi-v7a" APP_PLATFORM=android-21 NDK_DEBUG=0 APP_OPTIM=release
 
 mkdir -p ../Plugins/Android/libs/armeabi-v7a
 cp libs/armeabi-v7a/libtolua.so ../Plugins/Android/libs/armeabi-v7a
--- build_arm64.sh
+++ build_arm64.sh
@@ -12,13 +12,13 @@ NDKCC=$NDKP/aarch64-linux-android$NDKABI-clang
 NDKCROSS=$NDKP/aarch64-linux-android-
 
 make clean
-make HOST_CC="clang -m64" CROSS=$NDKCROSS STATIC_CC=$NDKCC DYNAMIC_CC="$NDKCC -fPIC" TARGET_LD=$NDKCC TARGET_AR="$NDKP/llvm-ar rcus" TARGET_STRIP="$NDKP/llvm-strip" TARGET_SYS=Linux TARGET_FLAGS="--sysroot $NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot $NDKARCH"
+make HOST_CC="clang -m64" CROSS=$NDKCROSS STATIC_CC=$NDKCC DYNAMIC_CC="$NDKCC -fPIC -O3" TARGET_LD=$NDKCC TARGET_AR="$NDKP/llvm-ar rcus" TARGET_STRIP="$NDKP/llvm-strip" TARGET_SYS=Linux TARGET_FLAGS="--sysroot $NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot $NDKARCH"
 cp ./libluajit.a ../../android/jni/libluajit.a
 make clean
 
 cd ../../android
 $NDK/ndk-build clean APP_ABI="armeabi-v7a,x86,arm64-v8a" APP_PLATFORM=android-21
-$NDK/ndk-build APP_ABI="arm64-v8a" APP_PLATFORM=android-21
+$NDK/ndk-build APP_ABI="arm64-v8a" APP_PLATFORM=android-21 NDK_DEBUG=0 APP_OPTIM=release
 
 mkdir -p ../Plugins/Android/libs/arm64-v8a
 cp libs/arm64-v8a/libtolua.so ../Plugins/Android/libs/arm64-v8a
