d959b8a9b5ff7d202a21470a7b784a18d830cd43
xiangchen02 <18351967270@163.com>
2024-12-03 20:07:18 +0800

    fix: 修改编译配置，使用标准的 NDK 工具链路径和编译器选项

--- build_arm64.sh
+++ build_arm64.sh
@@ -12,7 +12,7 @@ NDKINC="-I$NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include -I$NDK/
 NDKDEFS="-D__ANDROID_API__=$NDKABI -D__ANDROID__ -DANDROID"
 
 make clean
-make HOST_CC="gcc -m64" CROSS=$NDKP TARGET_CC="$NDKP/aarch64-linux-android$NDKABI-clang" TARGET_LD="$NDKP/aarch64-linux-android$NDKABI-clang" TARGET_AR="$NDKP/llvm-ar rcus" TARGET_STRIP="$NDKP/llvm-strip" TARGET_SYS=Linux TARGET_FLAGS="--sysroot $NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot $NDKARCH" TARGET_CFLAGS="-fPIC $NDKINC $NDKDEFS"
+make CROSS=$NDKP TARGET_CC="$NDKP/aarch64-linux-android$NDKABI-clang" TARGET_LD="$NDKP/aarch64-linux-android$NDKABI-clang" TARGET_AR="$NDKP/llvm-ar rcus" TARGET_STRIP="$NDKP/llvm-strip" TARGET_SYS=Linux TARGET_FLAGS="--sysroot $NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot $NDKARCH" TARGET_CFLAGS="-fPIC $NDKINC $NDKDEFS"
 cp ./libluajit.a ../../android/jni/libluajit.a
 make clean
 
